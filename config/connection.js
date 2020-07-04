d4466d7b1145ad4cd2ec3f071aec6e230b20642f
Soul <sornwilliam@gmail.com>
2020-07-03 19:43:06 -0500

    JAWSDB added

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 const mysql = require("mysql");
+const connection;
 
-const connection = mysql.createConnection({
-    host:"localhost", //CAN REPLACE WITH VARS ON HEROKU w/ process.env.SOMETHING
-    port: 3306,
-    user:"root",
-    password:"22087005552493",
-    database:"burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '22087005552493',
+        database: 'burgers_db'
+    });
+};
 
 connection.connect(function(err){
     if (err) {
